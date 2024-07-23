a359ffa83e27c7e50f2086cdfb41db2d23490484
Amir Hossein keshavarz <hduck129@gmail.com>
2024-07-23 02:16:06 -0700

    build(jsFile): Create school classes and student list with Array
    +Use Array and object to list a classes and students.
    +use console.log() for show informations

--- school-project/js/script.js
+++ school-project/js/script.js
@@ -1,102 +1,51 @@
-// create a school object with classA and classB
+// create a school object with classA and classB(Array)
 let school = {
-  classA: {
-    student1: {
-      Name: "John",
-      Age: 10,
-      phoneNumber: "091234556677",
-      address: "BU/02",
-      score: "10",
+  // classsA Array(list of students)
+  classes: [
+    // create information os Teacher and classA and Students.
+    {
+      Name: "Class A",
+      Teacher: "Mr.khateri",
+      students: [
+        {
+          Name: "Alex",
+          Age: 18,
+          Address: "bu/85",
+          Phone: "8658740567",
+          Score: 18,
+        },
+        {
+          Name: "Amer",
+          Age: 20,
+          Address: "bu/67",
+          Phone: "94879048",
+          Score: 20,
+        },
+      ],
     },
-    student2: {
-      Name: "Alex",
-      Age: 11,
-      phoneNumber: "091234556677",
-      address: "BU/05",
-      score: "14",
+    // create information os Teacher and classA and Students.
+    {
+      Name: "Class B",
+      Teacher: "Mr.Heydari",
+      students: [
+        {
+          Name: "Mahsa",
+          Age: 17,
+          Address: "bu/09",
+          Phone: "767856564",
+          Score: 19,
+        },
+        {
+          Name: "Misha",
+          Age: 18,
+          Address: "bu/03",
+          Phone: "767856564",
+          Score: 17,
+        },
+      ],
     },
-    student3: {
-      Name: "Jim",
-      Age: 10,
-      phoneNumber: "091234556677",
-      address: "BU/06",
-      score: "20",
-    },
-  },
-  classB: {
-    student1: {
-      Name: "Martinez",
-      Age: 15,
-      phoneNumber: "091234556677",
-      address: "BU/09",
-      score: "13",
-    },
-    student2: {
-      Name: "Jane",
-      Age: 16,
-      phoneNumber: "091234556677",
-      address: "BU/10",
-      score: "17",
-    },
-    student3: {
-      Name: "Oscar",
-      Age: 15,
-      phoneNumber: "091234556677",
-      address: "BU/19",
-      score: "20",
-    },
-  },
+  ],
 };
-// show class name
-console.log("CLASS A:");
-console.log(" ");
-// show information of student1 ClassA (Name+Age+phoneNumber+address+score)
-console.log("Name:" + school.classA.student1.Name);
-console.log("Age:" + school.classA.student1.Age);
-console.log("phoneNumber:" + school.classA.student1.phoneNumber);
-console.log("Addres:" + school.classA.student1.address);
-console.log("Score:" + school.classA.student1.score);
-console.log(" ");
 
-// show information of student2 ClassA (Name+Age+phoneNumber+address+score)
-console.log("Name:" + school.classA.student2.Name);
-console.log("Age:" + school.classA.student2.Age);
-console.log("phoneNumber:" + school.classA.student2.phoneNumber);
-console.log("Addres:" + school.classA.student2.address);
-console.log("Score:" + school.classA.student2.score);
-console.log(" ");
-
-// show information of student3 ClassA (Name+Age+phoneNumber+address+score)
-console.log("Name:" + school.classA.student3.Name);
-console.log("Age:" + school.classA.student3.Age);
-console.log("phoneNumber:" + school.classA.student3.phoneNumber);
-console.log("Addres:" + school.classA.student3.address);
-console.log("Score:" + school.classA.student3.score);
-console.log(" ");
-
-//   show class name
-console.log(" ");
-console.log("CLASS B:");
-// show information of student1 ClassB (Name+Age+phoneNumber+address+score)
-console.log("Name:" + school.classB.student1.Name);
-console.log("Age:" + school.classB.student1.Age);
-console.log("phoneNumber:" + school.classB.student1.phoneNumber);
-console.log("Addres:" + school.classB.student1.address);
-console.log("Score:" + school.classB.student1.score);
-console.log(" ");
-
-// show information of student2 ClassB (Name+Age+phoneNumber+address+score)
-console.log("Name:" + school.classB.student2.Name);
-console.log("Age:" + school.classB.student2.Age);
-console.log("phoneNumber:" + school.classB.student2.phoneNumber);
-console.log("Addres:" + school.classB.student2.address);
-console.log("Score:" + school.classB.student2.score);
-console.log(" ");
-
-// show information of student3 ClassB (Name+Age+phoneNumber+address+score)
-console.log("Name:" + school.classB.student3.Name);
-console.log("Age:" + school.classB.student3.Age);
-console.log("phoneNumber:" + school.classB.student3.phoneNumber);
-console.log("Addres:" + school.classB.student3.address);
-console.log("Score:" + school.classB.student3.score);
-console.log(" ");
+// show classes students name and informations
+console.log(school.classes);
