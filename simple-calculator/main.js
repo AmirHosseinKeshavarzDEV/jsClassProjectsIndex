93846cf378d679c1b28a88db29d0af74b59a8a02
Amir Hossein keshavarz <hduck129@gmail.com>
2024-08-02 12:44:24 -0700

    Build(JS FILE):create calculator for show simple  math operation
    + use propmt() for get data numbers.
    + use to variables for get datas with prompt().
    +use  5 console.log for show diffrent consequence.
    :100:

---
+++ simple-calculator/main.js
@@ -0,0 +1,26 @@
+// simple Calculator
+
+// get input 1 with let function
+
+let entranceA = parseInt(prompt("Enter First Number:"));
+
+// get input 1 with let function
+
+let entranceB = parseInt(prompt("Enter Second Number:"));
+
+// show math operation in log console
+
+// show Total
+console.log("Total:", entranceA + entranceB);
+
+// show subtraction
+console.log("subtraction:", entranceA - entranceB);
+
+// show multiplication
+console.log("multiplication:", entranceA * entranceB);
+
+// show division
+console.log("division:", entranceA / entranceB);
+
+// show difference
+console.log("difference:", entranceA % entranceB);
